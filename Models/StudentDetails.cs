504796e624ab3366dde792800d6e702c679435a2
lokes <lokes@RAJS-PC>
2021-06-03 11:40:45 +0530

    Add project files.

---
+++ Models/StudentDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentAsignMVCBreakout.Models
+{
+    public class StudentDetails
+    {
+        public int Rollno { get; set; }
+        public String Name { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String Country { get; set; }
+    }
+}
